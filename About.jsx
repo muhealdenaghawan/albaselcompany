e53f603af42f9ef0b97c6aeab3766d3de2e2a14b
Ryzen Technology <mhealdenagwan@gmail.com>
2025-04-28 10:45:27 +0300

    createing code in this files

---
+++ About.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Footer from './common/Footer'
+import Header from './common/Header'
+
+const About = () => {
+  return (
+   <>
+    <Header/>
+    <main>
+      About
+    </main>
+    <Footer/>
+   </>
+  )
+}
+
+export default About
