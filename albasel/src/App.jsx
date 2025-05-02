bb923ee717ee9844e2010fece152ae74193fa396
Ryzen Technology <mhealdenagwan@gmail.com>
2025-05-02 21:59:51 +0300

    رفع المجلد كامل

---
+++ albasel/src/App.jsx
@@ -0,0 +1,59 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Route , Routes } from 'react-router-dom';
+import Home from './components/frontend/Home';
+import About from './components/frontend/About';
+import './assets/css/style.scss';
+import Services from './components/frontend/Services';
+import Projects from './components/frontend/Projects';
+import Blogs from './components/frontend/Blogs';
+import ContactUs from './components/frontend/ContactUs';
+import Login from './components/backend/Login';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Dashboard from './components/backend/Dashboard';
+import RequireAuth from './components/common/RequireAuth';
+
+
+// import React from 'react';
+
+import { AuthProvider } from './components/backend/context/Auth'; // تأكد من الاستيراد هنا
+
+function App() {
+
+  return (
+    <>
+    <AuthProvider>
+      <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<Home/>} />
+            <Route path='/about' element={<About/>} />
+            <Route path='/services' element={<Services/>} />
+            <Route path='/projects' element={<Projects/>} />
+            <Route path='/blogs' element={<Blogs/>} />
+            <Route path='/contact' element={<ContactUs/>} />
+            <Route path='/admin/login' element={<Login/>} />
+
+            
+            <Route path='/admin/dashboard' element={
+              
+              <RequireAuth>
+                <Dashboard/>
+              </RequireAuth>
+              } />
+
+          </Routes>
+      </BrowserRouter>
+      </AuthProvider> 
+
+
+      <ToastContainer 
+        position="top-center"
+      />
+    </>
+  )
+}
+
+export default App
