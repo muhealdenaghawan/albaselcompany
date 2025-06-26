5def4d04e2b47dbfd9907658d1c85de7a1a3e5f0
Rawan-Alhabashi3 <rawanhabashi2003@gmail.com>
2025-06-26 20:13:04 +0300

    last code

---
+++ lang/en/api.php
@@ -0,0 +1,51 @@
+<?php
+
+return [
+
+    "No Services Found" => "No Services Found",
+    "Service List" => "Service List",
+    "Service Created Successfully" => "Service Created Successfully",
+    "Service Updated Successfully" => "Service Updated Successfully",
+    "Service Deleted Successfully" => "Service Deleted Successfully",
+
+    "No Users Found" => "No Users Found",
+    "User List" => "User List",
+    "User Created Successfully" => "User Created Successfully",
+    "User Updated Successfully" => "User Updated Successfully",
+    "User Deleted Successfully" => "User Deleted Successfully",
+
+    "Cannot create a user with admin role." => "Cannot create a user with admin role.",
+    "Cannot update an admin user." => "Cannot update an admin user.",
+    "Cannot delete an admin user." => "Cannot delete an admin user.",
+
+    "Email not found" => "Email not found",
+    "The provided credentials are incorrect." => "The provided credentials are incorrect.",
+    "Unauthenticated." => "Unauthenticated.",
+    "logged out" => "logged out",
+    "Admin Created Client" => "Admin Created Client",
+
+    "Unauthorized. Employees and Admins only." => "Unauthorized. Employees and Admins only.",
+    "Unauthorized. Admins only." => "Unauthorized. Admins only.",
+    "Material List" => "Material List",
+    "Material Created Successfully" => "Material Created Successfully",
+    "Material Updated Successfully" => "Material Updated Successfully",
+    "Material Deleted Successfully" => "Material Deleted Successfully",
+    "No Materials Found" => "No Materials Found",
+
+    "Project List" => "Project List",
+    "Project Created Successfully" => "Project Created Successfully",
+    "Project Updated Successfully" => "Project Updated Successfully",
+    "Project Deleted Successfully" => "Project Deleted Successfully",
+    "No Projects Found" => "No Projects Found",
+    "Cannot edit project unless it is in pending state." => "Cannot edit project unless it is in pending state.",
+    "Project Assigned Successfully" => "Project Assigned Successfully",
+    "Project Updated Successfully" => "Project Updated Successfully",
+
+    "Validation Error" => "Validation Error",
+    "Project service and materials stored successfully" => "Project service and materials stored successfully",
+    "Something went wrong" => "Something went wrong",
+    "No services or materials found for this project." => "No services or materials found for this project.",
+    "Project cost calculated successfully." => "Project cost calculated successfully.",
+    "Unauthorized. Employees and Admins only." => "Unauthorized. Employees and Admins only.",
+    
+];
---
+++ lang/en/web.php
@@ -0,0 +1,71 @@
+<?php
+
+return [
+    'about-us' => 'About Us',
+    'We are a contracting company specialized in executing residential, commercial, and industrial projects. With extensive experience and a professional team that combines engineering expertise with technical knowledge, we believe that quality starts with precise planning and ends with the delivery of a fully integrated project that satisfies the client. Our commitment to professionalism, transparency, and quality makes us the ideal partner to bring your architectural vision to life.' =>
+    'We are a contracting company specialized in executing residential, commercial, and industrial projects. With extensive experience and a professional team that combines engineering expertise with technical knowledge, we believe that quality starts with precise planning and ends with the delivery of a fully integrated project that satisfies the client. Our commitment to professionalism, transparency, and quality makes us the ideal partner to bring your architectural vision to life.',
+    'why-choose-us?' => 'Why Choose Us?',
+    'completing-projects-on-time' => 'Completing projects on time',
+    'At Al-Basel General Contracting Company, we are committed to delivering projects on time without delay. We believe that respecting timelines is the foundation of trust between us and our clients. That is why we develop precise, well-thought-out execution plans and adhere to them with utmost dedication.' =>
+    'At Al-Basel General Contracting Company, we are committed to delivering projects on time without delay. We believe that respecting timelines is the foundation of trust between us and our clients. That is why we develop precise, well-thought-out execution plans and adhere to them with utmost dedication.',
+    'Always here to serve you' => 'Always here to serve you',
+    'Our team is always available to communicate and respond to your inquiries throughout all stages of the project. Whether you need an update, technical consultation, or immediate support, we are here to serve you with efficiency and professionalism.'
+    => 'Our team is always available to communicate and respond to your inquiries throughout all stages of the project. Whether you need an update, technical consultation, or immediate support, we are here to serve you with efficiency and professionalism.',
+    'professionalism-and-high-responsibility' => 'professionalism-and-high-responsibility',
+    'We approach each project with the utmost professionalism and responsibility, from the planning stage to final delivery. We are committed to using the best engineering standards and the latest technologies to ensure quality work and customer satisfaction.'
+    => 'We approach each project with the utmost professionalism and responsibility, from the planning stage to final delivery. We are committed to using the best engineering standards and the latest technologies to ensure quality work and customer satisfaction.',
+    "contact-us" => "Contact Us",
+    "send" => "Send",
+    "email" => "Email",
+    "your-name" => "Your Name",
+    "phone-number" => "Phone Number",
+    "message" => "Message",
+    "call" => "Call",
+    "quick-links" => "Quick Links",
+    "home" => "Home",
+    "services" => "Services",
+    "about" => "About",
+    "project" => "Project",
+    "testimonial" => "Testimonial",
+    "contact-us" => "Contact Us",
+    "sign-up-to-our-newsletter" => "Sign Up To Our Newsletter",
+    "subscribe" => "Subscribe",
+    "At our company, we offer a comprehensive range of contracting services covering all project phases, from concept to completion. We are committed to providing innovative engineering solutions, high-quality execution, and attention to the smallest details to ensure our clients satisfaction. Our services include general construction, building modeling, pre-construction services, and project management—all within a clear vision focused on adherence to deadlines, efficiency, and global engineering standards"
+    => "At our company, we offer a comprehensive range of contracting services covering all project phases, from concept to completion. We are committed to providing innovative engineering solutions, high-quality execution, and attention to the smallest details to ensure our clients satisfaction. Our services include general construction, building modeling, pre-construction services, and project management—all within a clear vision focused on adherence to deadlines, efficiency, and global engineering standards",
+    "Construction Services" => "Construction Services",
+    "Building Modeling" => "Building Modeling",
+    "We offer comprehensive construction services covering all phases of construction, from foundations to final completion. We adhere to the highest quality standards, using the latest technologies and materials to ensure projects are completed on time and within budget. Our team consists of engineers and technicians with extensive experience in implementing residential, commercial, and industrial projects."
+    => "We offer comprehensive construction services covering all phases of construction, from foundations to final completion. We adhere to the highest quality standards, using the latest technologies and materials to ensure projects are completed on time and within budget. Our team consists of engineers and technicians with extensive experience in implementing residential, commercial, and industrial projects.",
+    "We use Building Information Modeling (BIM) technologies to provide an accurate and comprehensive visualization of the project before construction begins. This technology helps reduce errors, improve efficiency, and save time and costs by digitally simulating all project elements and coordinating across various engineering disciplines." =>
+    "We use Building Information Modeling (BIM) technologies to provide an accurate and comprehensive visualization of the project before construction begins. This technology helps reduce errors, improve efficiency, and save time and costs by digitally simulating all project elements and coordinating across various engineering disciplines.",
+    "Pre-Construction" => "Pre-Construction",
+    "We provide you with full support during the pre-implementation phase, including feasibility studies, preliminary planning, cost estimation, risk assessment, and project timeline planning. Our goal is to set your project up for success from the start by making informed decisions and developing a clear implementation plan."
+    => "We provide you with full support during the pre-implementation phase, including feasibility studies, preliminary planning, cost estimation, risk assessment, and project timeline planning. Our goal is to set your project up for success from the start by making informed decisions and developing a clear implementation plan.",
+    "Project Management" => "Project Management",
+    "We provide professional project management services that include planning, supervision, coordination, and quality and cost control. We ensure smooth project execution and the achievement of your goals through efficient resource management and adherence to timelines and technical standards."
+    => "We provide professional project management services that include planning, supervision, coordination, and quality and cost control. We ensure smooth project execution and the achievement of your goals through efficient resource management and adherence to timelines and technical standards.",
+    "al-basel-general-contracting-company" => "al-basel-general-contracting-company",
+    "committed-to-delivering-residential-commercial-and-industrial-projects-with-top-quality" => "committed-to-delivering-residential-commercial-and-industrial-projects-with-top-quality",
+    "always-on-time-and-providing-professional-service-that-meets-your-expectations" => "always-on-time-and-providing-professional-service-that-meets-your-expectations",
+    "professionalism-and-high-responsibility" => "professionalism-and-high-responsibility",
+    "projects" => "projects",
+    "all" => "all",
+    "painting" => "painting",
+    "reconstruction" => "reconstruction",
+    "repairs" => "repairs",
+    "residential" => "residential",
+    "styling" => "styling",
+    "category_filter" => "Category Filter",
+    "interior_work" => "Interior Work",
+    "alteration_in_some_form_by_injected_humour_or_randomised_words_which_do_not_look_even_slightly_believable_if_you_are_going_to_use"
+    => "alteration in some form by injected humour or randomised words which do not look even slightly believable if you are going to use",
+    "testimonial" => "testimonial",
+    "morojink" => "Morojink",
+    "customer" => "Customer",
+    "Al-Basel Contracting exceeded our expectations. Their team was professional, responsive, and delivered our project on time with outstanding quality. We truly appreciate their commitment and attention to detail."
+    => "Al-Basel Contracting exceeded our expectations. Their team was professional, responsive, and delivered our project on time with outstanding quality. We truly appreciate their commitment and attention to detail.",
+    "" => "",
+    "" => "",
+    "" => "",
+    "" => "",
+];
